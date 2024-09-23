94ed319726879c19e3b6fd8e35153e1c052b4036
paulsea27115 <csb070115@gmail.com>
2024-09-23 16:00:04 +0900

    feat: 추천영화 등록된 영화 화면구현

--- movie_cinema/src/pages/index.tsx
+++ movie_cinema/src/pages/index.tsx
@@ -1,8 +1,30 @@
-import { ReactNode } from "react";
+import React, { ReactNode } from "react";
 import SearchableLayout from "@/components/searchable-layout";
+import style from "./index.module.css";
+import MovieItem from "@/components/movie-item";
+import movies from "@/mocks/movies.json";
 
 export default function Home() {
-  return <div>홈입니다.</div>;
+  return (
+    <div className={style.container}>
+      <section>
+        <h3>지금 가장 추천하는 영화</h3>
+        <div className={style.recommended_movies}>
+          {movies.map((movie, idx): ReactNode | null => {
+            return idx < 3 ? <MovieItem key={movie.id} {...movie} /> : null;
+          })}
+        </div>
+      </section>
+      <section>
+        <h3>등록된 모든 영화</h3>
+        <div className={style.all_movies}>
+          {movies.map((movie, idx): ReactNode => {
+            return <MovieItem key={movie.id} {...movie} />;
+          })}
+        </div>
+      </section>
+    </div>
+  );
 }
 
 Home.getLayout = (page: ReactNode) => {
