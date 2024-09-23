1bdcfb260715af7a45c9c9af88dbf720cbfb9713
paulsea27115 <csb070115@gmail.com>
2024-09-23 16:00:49 +0900

    feat: 검색한 영화 상세 페이지
    선택한 영화 상세 페이지 보기

--- movie_cinema/src/pages/movie/[id].tsx
+++ movie_cinema/src/pages/movie/[id].tsx
@@ -1,9 +1,31 @@
 import { useRouter } from "next/router";
+import movies from "@/mocks/movies.json";
+import style from "@/pages/movie/[id].module.css";
 
 export default function Page() {
   const router = useRouter();
 
   const { id } = router.query;
 
-  return <h1>{id + " 영화 상세페이지"}</h1>;
+  const movie = movies.filter((item) => String(item.id) === id)[0];
+
+  return (
+    <div className={style.container}>
+      <div
+        className={style.img_container}
+        style={{ backgroundImage: `url('${movie.posterImgUrl}')` }}
+      >
+        <img src={movie.posterImgUrl} alt="" />
+      </div>
+      <div className={style.content_container}>
+        <div className={style.title}>{movie.title}</div>
+        <div>
+          {movie.releaseDate} / {movie.genres} / {movie.runtime}
+        </div>
+        <div>{movie.company}</div>
+        <div className={style.subTitle}>{movie.subTitle}</div>
+        <div>{movie.description}</div>
+      </div>
+    </div>
+  );
 }
