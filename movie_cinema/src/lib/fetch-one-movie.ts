60c044db82f0b6d662f8e35135f4651820485bee
paulsea27115 <csb070115@gmail.com>
2024-09-27 00:05:22 +0900

    feat: 서버와 api 통신

---
+++ movie_cinema/src/lib/fetch-one-movie.ts
@@ -0,0 +1,21 @@
+import { MovieData } from "@/types";
+
+export default async function fetchOneMovie(
+  id: Number
+): Promise<MovieData | null> {
+  const url = `http://localhost:12345/movie/${id}`;
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error();
+    }
+
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+
+    return null;
+  }
+}
---
+++ movie_cinema/src/lib/fetch-recoMovies.ts
@@ -0,0 +1,19 @@
+import { MovieData } from "@/types";
+
+export default async function fetchRecoMovies(): Promise<MovieData[]> {
+  const url = `http://localhost:12345/movie/random`;
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error();
+    }
+
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+
+    return [];
+  }
+}
