3a16484032160040e783532df7f70c6f8c779b56
paulsea27115 <csb070115@gmail.com>
2024-09-27 00:05:55 +0900

    feat: 받은 값 적용

--- movie_cinema/src/pages/index.tsx
+++ movie_cinema/src/pages/index.tsx
@@ -2,23 +2,39 @@ import React, { ReactNode } from "react";
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
 import MovieItem from "@/components/movie-item";
-import movies from "@/mocks/movies.json";
+import fetchMovies from "@/lib/fetch-movies";
+import fetchRecoMovies from "@/lib/fetch-recoMovies";
+import { InferGetServerSidePropsType } from "next";
 
-export default function Home() {
+export const getServerSideProps = async () => {
+  const [allMovies, recoMovies] = await Promise.all([
+    fetchMovies(),
+    fetchRecoMovies(),
+  ]);
+
+  return {
+    props: { allMovies, recoMovies },
+  };
+};
+
+export default function Home({
+  allMovies,
+  recoMovies,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 가장 추천하는 영화</h3>
         <div className={style.recommended_movies}>
-          {movies.map((movie, idx): ReactNode | null => {
-            return idx < 3 ? <MovieItem key={idx + 1} {...movie} /> : null;
+          {recoMovies.map((movie, idx): ReactNode | null => {
+            return <MovieItem key={idx + 1} {...movie} />;
           })}
         </div>
       </section>
       <section>
         <h3>등록된 모든 영화</h3>
         <div className={style.all_movies}>
-          {movies.map((movie, idx): ReactNode => {
+          {allMovies.map((movie, idx): ReactNode => {
             return <MovieItem key={idx + 1} {...movie} />;
           })}
         </div>
