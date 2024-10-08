fe67c16271869838bf0a13834d53d7dd4e795715
paulsea27115 <csb070115@gmail.com>
2024-10-08 10:42:57 +0900

    chore: SEO 설정 및 배포 준비

--- movie_cinema/src/pages/index.tsx
+++ movie_cinema/src/pages/index.tsx
@@ -4,7 +4,8 @@ import style from "./index.module.css";
 import MovieItem from "@/components/movie-item";
 import fetchMovies from "@/lib/fetch-movies";
 import fetchRecoMovies from "@/lib/fetch-recoMovies";
-import { InferGetServerSidePropsType, InferGetStaticPropsType } from "next";
+import { InferGetStaticPropsType } from "next";
+import Head from "next/head";
 
 export const getStaticProps = async () => {
   const [allMovies, recoMovies] = await Promise.all([
@@ -14,6 +15,7 @@ export const getStaticProps = async () => {
 
   return {
     props: { allMovies, recoMovies },
+    revalidate: 3,
   };
 };
 
@@ -22,24 +24,35 @@ export default function Home({
   recoMovies,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <div className={style.container}>
-      <section>
-        <h3>지금 가장 추천하는 영화</h3>
-        <div className={style.recommended_movies}>
-          {recoMovies.map((movie, idx): ReactNode | null => {
-            return <MovieItem key={`recomovie-${movie.id}`} {...movie} />;
-          })}
-        </div>
-      </section>
-      <section>
-        <h3>등록된 모든 영화</h3>
-        <div className={style.all_movies}>
-          {allMovies.map((movie, idx): ReactNode => {
-            return <MovieItem key={`allmovie-${movie.id}`} {...movie} />;
-          })}
-        </div>
-      </section>
-    </div>
+    <>
+      <Head>
+        <title>한입씨네마</title>
+        <meta property="og:title" content="한입씨네마" />
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta
+          property="og:description"
+          content="한입씨네마에 등록된 영화들을 만나보세요."
+        />
+      </Head>
+      <div className={style.container}>
+        <section>
+          <h3>지금 가장 추천하는 영화</h3>
+          <div className={style.recommended_movies}>
+            {recoMovies.map((movie, idx): ReactNode | null => {
+              return <MovieItem key={`recomovie-${movie.id}`} {...movie} />;
+            })}
+          </div>
+        </section>
+        <section>
+          <h3>등록된 모든 영화</h3>
+          <div className={style.all_movies}>
+            {allMovies.map((movie, idx): ReactNode => {
+              return <MovieItem key={`allmovie-${movie.id}`} {...movie} />;
+            })}
+          </div>
+        </section>
+      </div>
+    </>
   );
 }
 
--- movie_cinema/src/pages/movie/[id].tsx
+++ movie_cinema/src/pages/movie/[id].tsx
@@ -1,11 +1,8 @@
 import style from "@/pages/movie/[id].module.css";
-import {
-  GetServerSidePropsContext,
-  InferGetServerSidePropsType,
-  InferGetStaticPropsType,
-} from "next";
+import { GetServerSidePropsContext, InferGetStaticPropsType } from "next";
 import fetchOneMovie from "@/lib/fetch-one-movie";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 export const getStaticPaths = () => {
   return {
@@ -33,22 +30,30 @@ export default function Page({
   if (router.isFallback) return "로딩중 입니다.";
   if (movie === null) return "문제가 발생하였습니다. 다시 시도 해주세요";
   return (
-    <div className={style.container}>
-      <div
-        className={style.img_container}
-        style={{ backgroundImage: `url('${movie.posterImgUrl}')` }}
-      >
-        <img src={movie.posterImgUrl} alt="" />
-      </div>
-      <div className={style.content_container}>
-        <div className={style.title}>{movie.title}</div>
-        <div>
-          {movie.releaseDate} / {movie.genres} / {movie.runtime}
+    <>
+      <Head>
+        <title>{movie.title}</title>
+        <meta property="og:title" content={movie.title} />
+        <meta property="og:image" content={movie.posterImgUrl} />
+        <meta property="og:description" content={movie.description} />
+      </Head>
+      <div className={style.container}>
+        <div
+          className={style.img_container}
+          style={{ backgroundImage: `url('${movie.posterImgUrl}')` }}
+        >
+          <img src={movie.posterImgUrl} alt="" />
         </div>
-        <div>{movie.company}</div>
-        <div className={style.subTitle}>{movie.subTitle}</div>
-        <div>{movie.description}</div>
+        <div className={style.content_container}>
+          <div className={style.title}>{movie.title}</div>
+          <div>
+            {movie.releaseDate} / {movie.genres} / {movie.runtime}
+          </div>
+          <div>{movie.company}</div>
+          <div className={style.subTitle}>{movie.subTitle}</div>
+          <div>{movie.description}</div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
